73a7f93babb39fd7369cc8c9f4218ecebe916fe0
cwl <1697184654@qq.com>
2021-10-20 19:48:35 +0800

    整数类型转换

--- cmd/int_type_conver.go
+++ cmd/int_type_conver.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
@@ -10,8 +9,10 @@ func main() {
 	fmt.Println("类型转换：")
 	fmt.Scanln(&x)
 
-	fmt.Println("二进制：", strconv.FormatInt(x, 2))
-	fmt.Println("八进制：", strconv.FormatInt(x, 8))
-	fmt.Println("十六进制：", strconv.FormatInt(x, 16))
+	//strconv.FormatInt(x, 2)  整数转二进制
+	fmt.Printf("二进制原码：%b \n", x)
+	fmt.Printf("二进制反码：%b \n", 1^1)
+	fmt.Printf("八进制：%o \n", x)
+	fmt.Printf("十六进制：%x \n", x)
 
 }
